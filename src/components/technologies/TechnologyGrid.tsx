ffaa68913218327545c448ef1f91e1b86be80215
srfarukahmed <srfarukahmed@gmail.com>
2026-09-13 19:41:39 +0600

    Fixed the technologies

--- src/components/technologies/TechnologyGrid.tsx
+++ src/components/technologies/TechnologyGrid.tsx
@@ -19,10 +19,10 @@ const TechnologyGrid = ({
 }: TechnologyGridProps) => {
   return (
     <div className="  ">
-      <h1 className="text-2xl font-bold text-slate-900 sm:text-3xl">
+      <h1 className="ml-8 text-2xl font-bold text-slate-900 sm:text-3xl">
         Explore the <span className="text-pink-600">Technologies</span>
       </h1>
-      <p className="mt-1  text-sm text-gray-500">
+      <p className="ml-8  mt-1  text-sm text-gray-500">
         Pick one technology per category to build your ideal stack.
       </p>
 
